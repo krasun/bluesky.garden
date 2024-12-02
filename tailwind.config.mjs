fb7022cc6e272239f0c3dddc9f7670e7e77df857
Piotr Kulpinski <piotr@kulpinski.pl>
2024-12-03 00:17:48 +0100

    Refresh teh design of the homepage and prepare some reusable components for new pages

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "hsl(var(--color-primary) / <alpha-value>)",
+        background: "hsl(var(--color-background) / <alpha-value>)",
+        muted: "hsl(var(--color-muted) / <alpha-value>)",
+        secondary: "hsl(var(--color-secondary) / <alpha-value>)",
+        foreground: "hsl(var(--color-foreground) / <alpha-value>)",
+        card: "hsl(var(--color-card) / <alpha-value>)",
+        "card-dark": "hsl(var(--color-card-dark) / <alpha-value>)",
+        border: "hsl(var(--color-border) / <alpha-value>)",
+        "border-dark": "hsl(var(--color-border-dark) / <alpha-value>)",
+      },
+    },
+  },
+  plugins: [],
 }
