91a1c953644959fd859a65b1b55d9f7c897007c1
Dmytro Krasun <krasun.net@gmail.com>
2024-12-09 11:51:27 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind"
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-})
+    site: "https://bluesky.garden",
+    integrations: [
+        tailwind(),
+        sitemap({
+            // add only dedicated tool and category pages to the sitemap
+            filter: (page) =>
+                page.includes("tools/") || page.includes("categories/"),
+        }),
+    ],
+});
