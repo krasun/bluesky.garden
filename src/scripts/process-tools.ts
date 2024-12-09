9f39d1c8445f4fe7eb4760b461dfdf2303963502
Dmytro Krasun <krasun.net@gmail.com>
2024-12-09 12:16:31 +0200

    Fetch and store favicons locally

--- src/scripts/process-tools.ts
+++ src/scripts/process-tools.ts
@@ -1,3 +1,4 @@
+import slugify from "@sindresorhus/slugify";
 import * as fs from "fs";
 
 import tools from "~/tools.json";
@@ -9,10 +10,40 @@ for (const tool of tools) {
         console.log(`Skipping ${tool.name} because it already exists...`);
         continue;
     }
-
     index.add(tool.url);
+
+    const slug = slugify(tool.name, { decamelize: true });
+
+    let resolvedFaviconAssetPath: string | undefined =
+        `assets/favicons/tools/${slug}.png`;
+    const faviconPath = `./src/${resolvedFaviconAssetPath}`;
+    if (fs.existsSync(faviconPath)) {
+        console.log(`Favicon already exists for ${tool.name}, skipping...`);
+        resolvedFaviconAssetPath = undefined;
+    } else {
+        console.log(`Fetching favicon for ${tool.name}...`);
+        const faviconUrl = `https://www.google.com/s2/favicons?sz=128&domain_url=${tool.url}`;
+        const response = await fetch(faviconUrl);
+        if (!response.ok) {
+            console.error(
+                `Failed to fetch favicon for ${tool.name}, status: ${response.status}`
+            );
+            resolvedFaviconAssetPath = undefined;
+        } else {
+            const imageBlob = await response.blob();
+            fs.writeFileSync(
+                faviconPath,
+                new Uint8Array(await imageBlob.arrayBuffer())
+            );
+        }
+    }
+
     processedTools.push({
         ...tool,
+        faviconPath:
+            "faviconPath" in tool && tool.faviconPath
+                ? tool.faviconPath
+                : resolvedFaviconAssetPath,
     });
 }
 
